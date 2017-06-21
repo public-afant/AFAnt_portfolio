e0ccff6d5690e47f0537a485c1f9f1c9b8feea07
Yohan Yoo <wh9721@gmail.com>
2017-06-22 01:08:21 +0900

    Google Analytics.

--- afant_portfolio/settings.py
+++ afant_portfolio/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'blog.context_processors.google_analytics',
+                'blog.context_processors.google_analytics.google_analytics',
             ],
         },
     },
